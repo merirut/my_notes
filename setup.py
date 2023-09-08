b78726848f6c7b207ef806d338389aec9a57a035
Maria Abdullaeva <abdullaevamariya2004@gmail.com>
2023-09-08 10:07:18 +0300

    replaced setup.py with pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='my_notes',
-    version='0.1.0',
-    packages=find_packages(),
-    package_data={'my_notes': ['*.ui']},
-    install_requires='PyQt5'
-)
